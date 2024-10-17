b0ae707d568c8e42c382bcb43be34e12a87a77c6
Yazzine02 <yassine.qat02@gmail.com>
2024-10-17 15:43:29 -0400

    module 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,40 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+    
+    # Additional fields can be added here as needed
+    
+    def __str__(self):
+        return self.name
 
+# Car Model model
+class CarModel(models.Model):
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    CAR_TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Foreign key to CarMake
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default='SUV')
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023)
+        ]
+    )
+    
+    # Additional fields can be added here as needed
+    
+    def __str__(self):
+        return f"{self.name} ({self.car_make.name}) - {self.year}"
